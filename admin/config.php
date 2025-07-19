6de58705a3dee26a5db249bb8c3238a26046aaea
aditya bagul <baguladitya66@gmail.com>
2025-07-20 00:12:10 +0530

    updating all structure

---
+++ admin/config.php
@@ -0,0 +1,7 @@
+<?php
+//database credentials
+$host = 'sql101.infinityfree.com';
+$username = 'if0_38281362';
+$password = 'Aditya9284';
+$dbname = 'if0_38281362_shivsar';
+?>
--- admin/db.php
+++ admin/db.php
@@ -1,14 +1,9 @@
 <?php
-
-$host = 'sql101.infinityfree.com';  
-$username = 'if0_38281362';
-$password = 'Aditya9284';
-$dbname = 'if0_38281362_shivsar';  
+require_once 'config.php'; // Load credentials
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
